2c7cd1b7ee2f31ca8885ca45e036a8b10215be81
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-01-13 13:32:04 +0900

    1310 improvement
    refer to https://leetcode.com/problems/xor-queries-of-a-subarray/discuss/470787/JavaC%2B%2BPython-Straight-Forward-Solution

--- 1310-xor-queries-of-a-subarray/main.go
+++ 1310-xor-queries-of-a-subarray/main.go
@@ -3,20 +3,28 @@ package main
 import "fmt"
 
 func xorQueries(arr []int, queries [][]int) []int {
+	calc := make([]int, len(arr))
+	for i, e := range arr {
+		if i == 0 {
+			calc[i] = e
+		} else {
+			calc[i] = calc[i-1] ^ e
+		}
+	}
+
 	out := make([]int, len(queries))
 	for i, q := range queries {
-		s := arr[q[0] : q[1]+1]
-		res := 0
-		for _, n := range s {
-			res = res ^ n
+		if q[0] > 0 {
+			out[i] = calc[q[0]-1] ^ calc[q[1]]
+		} else {
+			out[i] = calc[q[1]]
 		}
-		out[i] = res
 	}
 	return out
 }
 
 func main() {
-	arr := []int{4, 8, 2, 10}
-	queries := [][]int{{2, 3}, {1, 3}, {0, 0}, {0, 3}}
+	arr := []int{15, 8, 8, 8, 15}
+	queries := [][]int{{2, 2}, {3, 3}}
 	fmt.Println(xorQueries(arr, queries))
 }
