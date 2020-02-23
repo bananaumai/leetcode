141ae6a67bd4c6ca880087f4332339eb34d1e1d0
YUTA SHIMAKAWA <banana.umai@gmail.com>
2020-02-23 21:12:57 +0900

    n-queens alt (cps and loop)

--- 51-n-queens/nqueens.go
+++ 51-n-queens/nqueens.go
@@ -9,7 +9,11 @@ func main() {
 func solveNQueens(n int) [][]string {
 	var result [][]string
 
-	for _, qs := range nQueens(n, n-1) {
+	qss := nQueensRec(n, n-1)
+	//qss := nQueensRecCPS(n, 0, [][]int{})
+	//qss := nQueensLoop(n)
+
+	for _, qs := range qss {
 		var ss []string
 		for _, q := range qs {
 			s := ""
@@ -28,7 +32,7 @@ func solveNQueens(n int) [][]string {
 	return result
 }
 
-func nQueens(n int, m int) [][]int {
+func nQueensRec(n int, m int) [][]int {
 	var qss [][]int
 
 	if m == 0 {
@@ -38,7 +42,7 @@ func nQueens(n int, m int) [][]int {
 		return qss
 	}
 
-	for _, qs := range nQueens(n, m-1) {
+	for _, qs := range nQueensRec(n, m-1) {
 		for newQ := 0; newQ < n; newQ++ {
 			addable := true
 			for k, q := range qs {
@@ -54,6 +58,79 @@ func nQueens(n int, m int) [][]int {
 				qss = append(qss, newQs)
 			}
 		}
+	}
+
+	return qss
+}
+
+func nQueensRecCPS(n int, m int, acc [][]int) [][]int {
+	if m == n {
+		return acc
+	}
+
+	var qss [][]int
+	var next = m+1
+
+	if m == 0 {
+		for q := 0; q < n; q++ {
+			qss = append(qss, []int{q})
+		}
+		return nQueensRecCPS(n, next, qss)
+	}
+
+	for _, qs := range acc {
+		for newQ := 0; newQ < n; newQ++ {
+			addable := true
+			for k, q := range qs {
+				diff := m-k
+				switch newQ {
+				case q, q+diff, q-diff:
+					addable = false
+				}
+			}
+			if addable {
+				newQs := append([]int{}, qs...)
+				newQs = append(newQs, newQ)
+				qss = append(qss, newQs)
+			}
+		}
+	}
+
+	return nQueensRecCPS(n, next, qss)
+}
+
+func nQueensLoop(n int) [][]int {
+	var qss [][]int
+
+	for m := 0; m < n; m++ {
+		if m == 0 {
+			for q := 0; q < n; q++ {
+				qss = append(qss, []int{q})
+			}
+			continue
+		}
+
+		var newQss [][]int
+
+		for _, qs := range qss {
+			for newQ := 0; newQ < n; newQ++ {
+				addable := true
+				for k, q := range qs {
+					diff := m-k
+					switch newQ {
+					case q, q+diff, q-diff:
+						addable = false
+					}
+				}
+				if addable {
+					newQs := append([]int{}, qs...)
+					newQs = append(newQs, newQ)
+					newQss = append(newQss, newQs)
+				}
+			}
+		}
+
+		qss = newQss
 	}
 
 	return qss
